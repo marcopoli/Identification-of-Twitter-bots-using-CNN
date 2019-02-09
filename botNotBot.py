8070b8cec7fa8248f8e066913d1b372d566bb830
Marco Polignano <marco.polignano@uniba.it>
2019-02-09 19:09:03 +0100

    Server Version con Cross-Vaidation

--- botNotBot.py
+++ botNotBot.py
@@ -3,141 +3,143 @@ import numpy as np
 os.listdir()
 pathEn = "en"
 import joblib
+matrixTweetsEmb = joblib.load('matrixTweetsEmb_ALT.dump')
+listaClasses = joblib.load('listaClasses.dump')
+#
+# import resource
+#
+# def set_memory_limit(memory_kilobytes):
+#     # ru_maxrss: peak memory usage (bytes on OS X, kilobytes on Linux)
+#     usage_kilobytes = lambda: resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+#     rlimit_increment = 1024 * 1024
+#     resource.setrlimit(resource.RLIMIT_DATA, (rlimit_increment, resource.RLIM_INFINITY))
+#
+#     memory_hog = []
+#
+#     while usage_kilobytes() < memory_kilobytes:
+#         try:
+#             for x in range(100):
+#                 memory_hog.append('x' * 400)
+#         except MemoryError as err:
+#             rlimit = resource.getrlimit(resource.RLIMIT_DATA)[0] + rlimit_increment
+#             resource.setrlimit(resource.RLIMIT_DATA, (rlimit, resource.RLIM_INFINITY))
+#
+# set_memory_limit(15000 * 1024)  # 50 mb
+#
+# # First of all, we will create a procedure to be tested on a single file.
+# #
+# # After this first step will be completed, we will extend this procedure to create a complete dataframe
+#
+# testfile = "en/1a5b808546838869bc39cebdbad951e3.xml"
+#
+# import pandas as pd
+# import xml.etree.ElementTree as ET
+# import io
+#
+# def iter_docs(author):
+#     '''This function extracts the text and the language from the XML'''
+#     author_attr = author.attrib
+#     for doc in author.iter('document'):
+#         doc_dict = author_attr.copy()
+#         doc_dict.update(doc.attrib)
+#         doc_dict['data'] = doc.text
+#         yield doc_dict
+#
+# xml_data = open(testfile, "r") # Opening the text file
+# etree = ET.parse(xml_data) # Create an ElementTree object
+# df = pd.DataFrame(list(iter_docs(etree.getroot()))) #Append the info to a pandas dataframe
+# df.head(10)
+# df['data'][0]
+# filename = testfile.split("/")[-1].split(".")[0]
+#
+# import time
+#
+# # Creating empty dataframe
+# dataEn = pd.DataFrame()
+#
+# # Monitoring time to load the files
+# start = time.time()
+#
+# for root, dirs, files in os.walk(pathEn):
+#     for file in files:
+#         if file == 'truth.txt':
+#             continue
+#         else:
+#             try:
+#                 pathToFile = root + '/' + file # Creating path
+#                 # print(pathToFile) # Just for debugging
+#                 xml_data = open(pathToFile, "r", encoding="utf8") # Opening the text file
+#                 etree = ET.parse(xml_data) # Create an ElementTree object
+#                 data = list(iter_docs(etree.getroot())) # Create a list of dictionaries with the data
+#                 filename = file.split(".")[0] # Get filename
+#                 for dictionary in data: # Loop through the dictionary
+#                     dictionary['ID'] = filename # Append filename
+#                 dataEn = dataEn.append(data)  # Append the list of dictionary to a pandas dataframe
+#
+#             # If the file is not valid, skip it
+#             except ValueError as e:
+#                 print(e)
+#                 continue
+#
+# end = time.time()
+# print("Total running time is", end - start)
+#
+# dataEn.head(10)
+#
+# dataEn['data'][0]
+# dataEn.describe()
+#
+# pathToLabels = "en/truth.txt"
+#
+# target = pd.read_csv(pathToLabels, sep=":::")
+# target.columns=['ID', 'botOrHuman', 'sex']
+# target.head(10)
+# target.describe()
+# mergedEnData = pd.merge(dataEn, target, on='ID')
+# mergedEnData.head(10)
+# mergedEnData.describe()
+#
+#
+# # # Deep Learning - CNN?
+# '''Creo la litsa degli ID, delle classi e dei tweets pr ogni ID'''
+# listaIds = [ ]
+#
+# matrixTweets = [ ]
+#
+# for index , x in mergedEnData.iterrows ( ):
+#     id = x[ 'ID' ]
+#     if id not in listaIds:
+#         newList = list ( )
+#         newList.append ( x[ 1 ] )
+#         matrixTweets.append ( newList )
+#         listaIds.append ( id )
+#         listaClasses.append ( x[ 3 ] )
+#     else:
+#         ls = matrixTweets[ listaIds.index ( id ) ]
+#         ls.append ( x[ 1 ] )
+#         matrixTweets[ listaIds.index ( id ) ] = ls
+#
+# print ( len ( listaIds ) )
 
-import resource
-
-def set_memory_limit(memory_kilobytes):
-    # ru_maxrss: peak memory usage (bytes on OS X, kilobytes on Linux)
-    usage_kilobytes = lambda: resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    rlimit_increment = 1024 * 1024
-    resource.setrlimit(resource.RLIMIT_DATA, (rlimit_increment, resource.RLIM_INFINITY))
-
-    memory_hog = []
-
-    while usage_kilobytes() < memory_kilobytes:
-        try:
-            for x in range(100):
-                memory_hog.append('x' * 400)
-        except MemoryError as err:
-            rlimit = resource.getrlimit(resource.RLIMIT_DATA)[0] + rlimit_increment
-            resource.setrlimit(resource.RLIMIT_DATA, (rlimit, resource.RLIM_INFINITY))
-
-set_memory_limit(15000 * 1024)  # 50 mb
-
-# First of all, we will create a procedure to be tested on a single file.
-# 
-# After this first step will be completed, we will extend this procedure to create a complete dataframe
-
-testfile = "en/1a5b808546838869bc39cebdbad951e3.xml"
-
-import pandas as pd
-import xml.etree.ElementTree as ET
-import io
-
-def iter_docs(author):
-    '''This function extracts the text and the language from the XML'''
-    author_attr = author.attrib
-    for doc in author.iter('document'):
-        doc_dict = author_attr.copy()
-        doc_dict.update(doc.attrib)
-        doc_dict['data'] = doc.text
-        yield doc_dict
-
-xml_data = open(testfile, "r") # Opening the text file
-etree = ET.parse(xml_data) # Create an ElementTree object 
-df = pd.DataFrame(list(iter_docs(etree.getroot()))) #Append the info to a pandas dataframe
-df.head(10)
-df['data'][0]
-filename = testfile.split("/")[-1].split(".")[0]
-
-import time
-
-# Creating empty dataframe
-dataEn = pd.DataFrame()
-
-# Monitoring time to load the files
-start = time.time()
-
-for root, dirs, files in os.walk(pathEn):
-    for file in files:
-        if file == 'truth.txt':
-            continue
-        else: 
-            try:
-                pathToFile = root + '/' + file # Creating path
-                # print(pathToFile) # Just for debugging
-                xml_data = open(pathToFile, "r", encoding="utf8") # Opening the text file
-                etree = ET.parse(xml_data) # Create an ElementTree object
-                data = list(iter_docs(etree.getroot())) # Create a list of dictionaries with the data
-                filename = file.split(".")[0] # Get filename
-                for dictionary in data: # Loop through the dictionary
-                    dictionary['ID'] = filename # Append filename
-                dataEn = dataEn.append(data)  # Append the list of dictionary to a pandas dataframe
-                
-            # If the file is not valid, skip it
-            except ValueError as e:
-                print(e)
-                continue
-            
-end = time.time()
-print("Total running time is", end - start)
-
-dataEn.head(10)
-
-dataEn['data'][0]
-dataEn.describe()
-
-pathToLabels = "en/truth.txt"
-
-target = pd.read_csv(pathToLabels, sep=":::")
-target.columns=['ID', 'botOrHuman', 'sex'] 
-target.head(10)
-target.describe()
-mergedEnData = pd.merge(dataEn, target, on='ID')
-mergedEnData.head(10)
-mergedEnData.describe()
-
-
-# # Deep Learning - CNN?
-'''Creo la litsa degli ID, delle classi e dei tweets pr ogni ID'''
-listaIds = [ ]
-listaClasses = [ ]
-matrixTweets = [ ]
-
-for index , x in mergedEnData.iterrows ( ):
-    id = x[ 'ID' ]
-    if id not in listaIds:
-        newList = list ( )
-        newList.append ( x[ 1 ] )
-        matrixTweets.append ( newList )
-        listaIds.append ( id )
-        listaClasses.append ( x[ 3 ] )
-    else:
-        ls = matrixTweets[ listaIds.index ( id ) ]
-        ls.append ( x[ 1 ] )
-        matrixTweets[ listaIds.index ( id ) ] = ls
-
-print ( len ( listaIds ) )
-
-'''Trasformo le entità, lascio le faccine, levo le stopword e se serve agli embeddings lemmatizzo'''
-
-from ekphrasis.dicts.emoticons import emoticons
-from ekphrasis.classes.preprocessor import TextPreProcessor
-from ekphrasis.classes.tokenizer import SocialTokenizer
-
-text_processor = TextPreProcessor (
-    # terms that will be normalized
-    normalize=[ 'email' , 'percent' , 'money' , 'phone' ,
-                'time' , 'url' , 'date' , 'number' ] ,
-    fix_html=True ,  # fix HTML tokens
-    segmenter="twitter" ,
-    corrector="twitter" ,
-    unpack_hashtags=True ,  # perform word segmentation on hashtags
-    unpack_contractions=True ,  # Unpack contractions (can't -> can not)
-    spell_correct_elong=True ,  # spell correction for elongated words
-    tokenizer=SocialTokenizer(lowercase=True).tokenize,
-    dicts=[ emoticons ]
-)
+# '''Trasformo le entità, lascio le faccine, levo le stopword e se serve agli embeddings lemmatizzo'''
+#
+# from ekphrasis.dicts.emoticons import emoticons
+# from ekphrasis.classes.preprocessor import TextPreProcessor
+# from ekphrasis.classes.tokenizer import SocialTokenizer
+#
+# text_processor = TextPreProcessor (
+#     # terms that will be normalized
+#     normalize=[ 'email' , 'percent' , 'money' , 'phone' ,
+#                 'time' , 'url' , 'date' , 'number' ] ,
+#     fix_html=True ,  # fix HTML tokens
+#     segmenter="twitter" ,
+#     corrector="twitter" ,
+#     unpack_hashtags=True ,  # perform word segmentation on hashtags
+#     unpack_contractions=True ,  # Unpack contractions (can't -> can not)
+#     spell_correct_elong=True ,  # spell correction for elongated words
+#     tokenizer=SocialTokenizer(lowercase=True).tokenize,
+#     dicts=[ emoticons ]
+# )
 
 # import gensim
 # google_300 = gensim.models.KeyedVectors.load_word2vec_format( "google_w2v_300.bin" , binary=True )
@@ -212,9 +214,7 @@ text_processor = TextPreProcessor (
 # print(matrixTweetsEmb.shape)
 
 #get_ipython().system('{sys.executable} -m pip install joblib')
-import joblib
-#joblib.dump(matrixTweetsEmb,'matrixTweetsEmb_4177_100_50_300.dump')
-matrixTweetsEmb = joblib.load('matrixTweetsEmb_ALT.dump')
+
 
 
 
